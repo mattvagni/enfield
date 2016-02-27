af6313a1b9779b47394bc98ff4255532cd68d2dd
Mathias Vagni <mathiasvagni@googlemail.com>
2016-02-27 16:46:03 +0000

    Changed logging symbol. #OCD

--- enfield/lib/log.js
+++ enfield/lib/log.js
@@ -3,14 +3,14 @@
 const chalk = require('chalk');
 
 /*
-Loggers for various levels.
+Loggers for various styles/levels.
  */
 module.exports = {
     success(message) {
-        console.log(chalk.bold.green('•') + ` ${message}`);
+        console.log(chalk.bold.green('∙ ') + message);
     },
     info(message) {
-        console.log(chalk.bold.yellow('•') + ` ${message}`);
+        console.log(chalk.bold.yellow('∙ ') + message);
     },
     error(message) {
         console.log(chalk.red(`\n${message}\n`));
